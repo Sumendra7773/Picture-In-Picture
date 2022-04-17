8ce25745b33436fc18892bd2130fbe5a75eddf9f
Sumendra7773 <98685181+Sumendra7773@users.noreply.github.com>
2022-04-17 15:59:07 +0530

    added an optional feature to load and play a video from system

--- Picture.js
+++ Picture.js
@@ -1,10 +1,19 @@
-const VideoElement = document.getElementById('Video');
-const button = document.getElementById('button');
+const VideoElement = document.getElementById('Video'),
+	button = document.getElementById('button'),
+	input = document.getElementById('video-input');
+
+function selectVideo() {
+	VideoElement.src = URL.createObjectURL(input.files[0]);
+	VideoElement.onloadedmetadata = () => {
+		VideoElement.play();
+	};
+}
 
 // Prompt to select media stream, pass to Video element ,then play
 async function selectMediaStream() {
 	try {
 		const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+
 		VideoElement.srcObject = mediaStream;
 		VideoElement.onloadedmetadata = () => {
 			VideoElement.play();
