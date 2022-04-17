3e155c08ccb20c93f015d9a64c3008b25add39ad
Sumendra7773 <98685181+Sumendra7773@users.noreply.github.com>
2022-04-17 14:37:04 +0530

    initial commit

---
+++ Picture.js
@@ -0,0 +1,29 @@
+const VideoElement = document.getElementById('Video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to Video element ,then play
+async function selectMediaStream() {
+	try {
+		const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+		VideoElement.srcObject = mediaStream;
+		VideoElement.onloadedmetadata = () => {
+			VideoElement.onplay();
+		};
+	} catch (error) {
+		// catch error here
+		console.log('whoops,error here', error);
+	}
+}
+
+button.addEventListener('click', async () =>
+{
+	// Disable Button
+	button.Disabled = true;
+	// Start Picture in Picture
+	await VideoElement.requestPictureInPicture();
+	// Reset Button
+	button.Disabled = false;
+});
+
+// on load
+selectMediaStream();
